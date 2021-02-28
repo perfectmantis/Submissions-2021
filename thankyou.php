2e88a680ee6fea75f0a5d2de08dcb97deb85164e
Ibtesam Uddin <ibtesamuddin95@gmail.com>
2021-02-28 13:41:32 +0500

    Database & DBMS Ecommerce Store
    Database & DBMS Ecommerce Store
    Group Members:
    Ibtesam Uddin (EP19101024)
    Syed Muhammad Dawar Imam (EP19101081)

---
+++ thankyou.php
@@ -0,0 +1,14 @@
+<?php
+include "header.php";
+?>
+
+
+<section class="section">
+<div class="container">	
+    <h3>Thank you for your order.</h3>
+</div>
+</section>	
+<?php
+include "newslettter.php";
+include "footer.php";
+?>
