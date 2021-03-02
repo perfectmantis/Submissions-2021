bc9f57de9f20ee67a3dcecffc8419be9e27f226a
usman <19101056.usman@gmail.com>
2021-03-02 11:37:52 +0500

    Web Mining and Knowledge Discovery
    Group Members
    EP19101056 Muhammad Usman Khan
    EP19101059 Muneeb Ashfaq

---
+++ FYP/Asp.Net Core Web Project/FYP/Models/DB/Status_Lists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models.DB
+{
+    public class Status_Lists
+    {
+        [Key]
+        public int id { get; set; }
+        public string status { get; set; }
+        public ICollection<Booking_Statuss> Booking_Statusses { get; set; }
+    }
+}
