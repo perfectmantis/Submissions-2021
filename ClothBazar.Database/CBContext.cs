fe58138187c52ce12cf47c6e8a7ef64cd92f94c2
Haris377 <Haris377@users.noreply.github.com>
2021-03-03 21:53:40 +0500

    initial commit

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,28 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+    public class CBContext : DbContext, IDisposable
+    {
+        public CBContext() : base("ClothBazarDB")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(50);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Config> Configurations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ ClothBazar.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ClothBazar.Services;
+using ClothBazar.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HomeViewModel model = new HomeViewModel();
+
+            model.FeaturedCategories = CategoriesService.Instance.GetFeaturedCategories();
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
