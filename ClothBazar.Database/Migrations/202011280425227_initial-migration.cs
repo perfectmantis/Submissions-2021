47e81ccf24b1636a1ea7f80f82b99bf7cc8c18de
MuhammadHaris <62830635+Haris377@users.noreply.github.com>
2021-03-04 22:20:58 +0500

    initial commit (#42)
    Co-authored-by: Haris377 <Haris377@users.noreply.github.com>

---
+++ ClothBazar.Database/Migrations/202011280425227_initial-migration.cs
@@ -0,0 +1,90 @@
+﻿namespace ClothBazar.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ImageURL = c.String(),
+                        isFeatured = c.Boolean(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CategoryID = c.Int(nullable: false),
+                        ImageURL = c.String(),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Categories", t => t.CategoryID, cascadeDelete: true)
+                .Index(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.Configs",
+                c => new
+                    {
+                        Key = c.String(nullable: false, maxLength: 128),
+                        Value = c.String(),
+                    })
+                .PrimaryKey(t => t.Key);
+            
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Quantity = c.Int(nullable: false),
+                        OrderID = c.Int(nullable: false),
+                        ProductID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Orders", t => t.OrderID, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductID, cascadeDelete: true)
+                .Index(t => t.OrderID)
+                .Index(t => t.ProductID);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        UserID = c.String(),
+                        OrderedAt = c.DateTime(nullable: false),
+                        Status = c.String(),
+                        TotalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "ProductID", "dbo.Products");
+            DropForeignKey("dbo.OrderItems", "OrderID", "dbo.Orders");
+            DropForeignKey("dbo.Products", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.OrderItems", new[] { "ProductID" });
+            DropIndex("dbo.OrderItems", new[] { "OrderID" });
+            DropIndex("dbo.Products", new[] { "CategoryID" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderItems");
+            DropTable("dbo.Configs");
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
