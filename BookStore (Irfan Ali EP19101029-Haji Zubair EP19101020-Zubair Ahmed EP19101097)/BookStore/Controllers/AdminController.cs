1c3f09586fcccf7389cffc34909146387ac498df
mimranfaruqi <44291943+mimranfaruqi@users.noreply.github.com>
2021-05-04 05:03:02 +0500

    Added more projects

---
+++ BookStore (Irfan Ali EP19101029-Haji Zubair EP19101020-Zubair Ahmed EP19101097)/BookStore/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class AdminController : Controller
+    {
+      
+        
+        //display list of books in table
+        [HttpGet]
+        public ActionResult BookList()
+        {
+            BookstoreEntities1 db = new BookstoreEntities1();
+            ViewBag.BookList = db.Books.ToList();
+            return View();
+        }
+
+        //Adding new book
+        [HttpGet]
+        public ActionResult Addbook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Addbook(BookStore.Models.Book book)
+        {
+            BookstoreEntities1 db = new BookstoreEntities1();
+
+            db.Books.Add(book);
+            db.SaveChanges();
+            return View();
+
+
+        }
+
+        //edit books
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            BookstoreEntities1 db = new BookstoreEntities1();
+            var category=db.Books.Find(id);
+
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(Book category)
+        {
+            BookstoreEntities1 db = new BookstoreEntities1();
+            db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("BookList");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            BookstoreEntities1 db = new BookstoreEntities1();
+            var category= db.Books.Find(id);
+            db.Books.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("BookList");
+        }
+        
+    }
+}
---
+++ BookStore (Irfan Ali EP19101029-Haji Zubair EP19101020-Zubair Ahmed EP19101097)/BookStore/Controllers/AllbookController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class AllbookController : Controller
+    {
+        //This is edit by Irfan
+        // GET: Image
+        public ActionResult Index()
+        {
+            BookstoreEntities1 dbt = new BookstoreEntities1();
+
+            
+            return View(dbt.Books.ToList());
+        }
+      
+
+        //search
+
+        BookstoreEntities1 db = new BookstoreEntities1();
+
+        public ActionResult Search(string Sortorder, string Sortby)
+        {
+            ViewBag.Sortorder = Sortorder;
+            var srch = db.Books.ToList();
+            switch (Sortorder)
+            {
+                case "Asc":
+                    {
+                        srch = srch.OrderBy(x => x.Name).ToList();
+                        break;
+
+                    }
+                case "Desc":
+                    {
+                        srch = srch.OrderByDescending(x => x.Name).ToList();
+                        break;
+                    }
+                default:
+                    {
+                        srch = srch.OrderBy(x => x.Name).ToList();
+                        break;
+
+                    }
+
+
+            }
+            return View(srch);
+        }
+        [HttpPost]
+        public ActionResult Search(string searchtxt, string Sortorder, string Sortby)
+        {
+            var srch = db.Books.ToList();
+            if (searchtxt != null)
+            {
+                srch = db.Books.Where(x => x.Name.Contains(searchtxt) || x.Author.Contains(searchtxt) || x.Condition.Contains(searchtxt) || x.Discription.Contains(searchtxt)).ToList();
+
+            }
+            return View(srch);
+        }
+
+        [HttpGet]
+        public ActionResult Download()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Buy()
+        {
+            return View();
+        }
+    }
+}
