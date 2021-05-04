47ff82fd42f126ad362ad84ec39dae805b38499b
mimranfaruqi <44291943+mimranfaruqi@users.noreply.github.com>
2021-05-04 15:25:55 +0500

    More projects

---
+++ ecommerce-web-application/solution/ClothBazar.Services/CategoriesService.cs
@@ -0,0 +1,122 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ClothBazar.Services
+{
+    public class CategoriesService                                              //BUSINESS LOGIC LAYER ONly few are completed work in going on
+    {
+        #region Singleton
+        public static CategoriesService Instance
+        {
+            get
+            {
+                if (instance == null) instance = new CategoriesService();
+
+                return instance;
+            }
+        }
+        private static CategoriesService instance { get; set; }
+        private CategoriesService()
+        {
+        }
+        #endregion
+
+        public Category GetCategory(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+        public int GetCategoriesCount(string search)
+        {
+            using (var context = new CBContext())
+            {
+                if (!string.IsNullOrEmpty(search))
+                {
+                    return context.Categories.Where(category => category.Name != null &&
+                         category.Name.ToLower().Contains(search.ToLower())).Count();
+                }
+                else
+                {
+                    return context.Categories.Count();
+                }                
+            }
+        }
+        public List<Category> GetAllCategories()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories
+                        .ToList();
+            }
+        }
+        public List<Category> GetCategories(string search, int pageNo)
+        {
+            int pageSize = 3;
+            using (var context = new CBContext())
+            {
+                if (!string.IsNullOrEmpty(search))
+                {
+                    return context.Categories.Where(category => category.Name != null &&
+                         category.Name.ToLower().Contains(search.ToLower()))
+                         .OrderBy(x => x.ID)
+                         .Skip((pageNo - 1) * pageSize)
+                         .Take(pageSize)
+                         .Include(x => x.Products)
+                         .ToList();
+                }
+                else
+                {
+                    return context.Categories
+                        .OrderBy(x => x.ID)
+                        .Skip((pageNo - 1) * pageSize)
+                        .Take(pageSize)
+                        .Include(x => x.Products)
+                        .ToList();
+                }
+            }
+        }
+        public List<Category> GetFeaturedCategories()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.Where(x => x.isFeatured && x.ImageURL != null).ToList();
+            }
+        }
+
+        public void SaveCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                var category = context.Categories.Where(x=>x.ID == ID).Include(x=>x.Products).FirstOrDefault();
+
+                context.Products.RemoveRange(category.Products); //first delete products of this category
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
