6064c8e9593927087463bef00d67796d777f7f2b
mimranfaruqi <44291943+mimranfaruqi@users.noreply.github.com>
2021-05-04 14:45:52 +0500

    Added one more project

---
+++ Web Mining and Knowledge Discovery(EP19101056 Muhammad Usman, EP19101059 Muneeb Ashfaq)/FYP/Asp.Net Core Web Project/FYP/Controllers/UpdateController.cs
@@ -0,0 +1,37 @@
+﻿using FYP.Models.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Controllers
+{
+    public class UpdateController : Controller
+    {
+        private fypDBContext db;
+        public UpdateController(fypDBContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index(string id)
+        {
+            long cid = long.Parse(id);
+            ViewBag.status_combo = db.status_list.AsNoTracking().ToList();
+            ViewBag.id = id;
+            return View();
+        }
+
+       [HttpPost]
+        public IActionResult Index(Booking_Statuss b)
+        {
+            DateTime d = DateTime.Now;
+            b.date = d;
+            b.time = DateTime.Now.ToShortTimeString();
+            db.booking_status.Add(b);
+            db.SaveChanges();
+            return RedirectToAction("Index" , "Track", new { id = b.booking_id });
+        }
+    }
+}
